f38a44ee4fc2b165c7a534663f4cec7537e2a2f8
Lucas Gavura <lgavu074@uottawa.ca>
2023-10-12 19:18:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/medibook/classes/Doctor.java
@@ -0,0 +1,8 @@
+package com.example.medibook.classes;
+public class Doctor extends User
+{
+
+    private String employeeNumber;
+    private String specialties;
+
+}
---
+++ app/src/main/java/com/example/medibook/classes/Patient.java
@@ -0,0 +1,7 @@
+package com.example.medibook.classes;
+public class Patient extends User
+{
+    private String healthCardNumber;
+
+
+}
