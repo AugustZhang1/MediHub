51c4e7763bbf84c895a76c2af5d9b3a137a029dd
Lucas Gavura <lgavu074@uottawa.ca>
2023-10-12 20:51:28 -0400

    Revert "Initial commit"
    This reverts commit f38a44ee4fc2b165c7a534663f4cec7537e2a2f8.

--- app/src/main/java/com/example/medibook/classes/Doctor.java
+++
@@ -1,8 +0,0 @@
-package com.example.medibook.classes;
-public class Doctor extends User
-{
-
-    private String employeeNumber;
-    private String specialties;
-
-}
--- app/src/main/java/com/example/medibook/classes/Patient.java
+++
@@ -1,7 +0,0 @@
-package com.example.medibook.classes;
-public class Patient extends User
-{
-    private String healthCardNumber;
-
-
-}
