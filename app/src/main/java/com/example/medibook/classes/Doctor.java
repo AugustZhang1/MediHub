c6982270369e81b829e27b68cd01fa94db42f4fe
August Zhang <azhan157@uottawa.ca>
2023-10-14 16:59:50 -0400

    added to proper file location of doctor and patient

--- app/src/main/java/com/example/medibook/classes/Doctor.java
+++ app/src/main/java/com/example/medibook/classes/Doctor.java
@@ -1,9 +1,15 @@
-package com.example.medibook.classes;
 public class Doctor extends User
 {
 
     private String employeeNumber;
     private String specialties;
+
+    public Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, String employeeNumber, String specialties)
+    {
+        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
+        this.employeeNumber = employeeNumber;
+        this.specialties = specialties;
+    }
 
 
 }
