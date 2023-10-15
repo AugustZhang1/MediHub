535fa6c8b0a67598c8adcfd48cacc2320e4ac994
Lucas Gavura <lgavu074@uottawa.ca>
2023-10-15 13:07:46 -0400

    Revert "change"
    This reverts commit 244f633bedc35b828f7521a7e33fa12062f03937.

--- app/src/main/java/com/example/medibook/MainActivity.java
+++ app/src/main/java/com/example/medibook/MainActivity.java
@@ -2,54 +2,13 @@ package com.example.medibook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import com.example.medibook.classes.User;
-import com.example.medibook.R;
-
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static List<User> userList = new ArrayList<>();
-
-    private Button btnDoc, btnPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        createViews();
-
-        btnPatient.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,PatientRegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        btnDoc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,DoctorRegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-    }
-
-
-
-    private void createViews(){
-        btnDoc = findViewById(R.id.asDoctor);
-        btnPatient = findViewById(R.id.asPatient);
     }
 }
