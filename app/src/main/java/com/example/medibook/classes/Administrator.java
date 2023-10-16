94c59243e8ed3d38300d7dacba752949266c3197
August Zhang <azhan157@uottawa.ca>
2023-10-15 20:47:20 -0400

    Admin class

---
+++ app/src/main/java/com/example/medibook/classes/Administrator.java
@@ -0,0 +1,9 @@
+package com.example.medibook.classes;
+
+public class Administrator extends User
+{
+    public Administrator(String firstName,  String lastName, String emailAddress, String accountPassword, String phoneNumber, String address )
+    {
+        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
+    }
+}
