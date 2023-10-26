04ab90f234a9ba142163f9a9689cfd4fbb4484bc
Sraucel <ryan.guo580@gmail.com>
2023-10-26 16:19:05 -0400

    adminconfirmreject

---
+++ app/src/main/java/com/example/medibook/AdminConfirmReject.java
@@ -0,0 +1,71 @@
+package com.example.medibook;
+
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class AdminConfirmReject extends AppCompatActivity {
+
+    private Button acceptBtn, rejectBtn, backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_confirm_reject);
+
+        createViews();
+
+        acceptBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent a = new Intent(AdminConfirmReject.this,MainActivity.class);
+                startActivity(a);
+
+            }
+
+        });
+
+        rejectBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent a = new Intent(AdminConfirmReject.this,MainActivity.class);
+                startActivity(a);
+            }
+
+
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a= new Intent(AdminConfirmReject.this,MainActivity.class);
+                startActivity(a);
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    public void createViews(){
+        acceptBtn = findViewById(R.id.buttonAccept);
+        rejectBtn = findViewById(R.id.buttonRejectAdmin);
+        backBtn = findViewById(R.id.backBtn);
+
+
+    }
+}
