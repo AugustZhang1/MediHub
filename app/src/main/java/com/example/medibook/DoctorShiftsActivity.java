d83583482e1803eb915870b70dda0ca4cc6ce057
Sraucel <ryan.guo580@gmail.com>
2023-11-12 17:54:30 -0500

    e

--- app/src/main/java/com/example/medibook/DoctorShiftsActivity.java
+++ app/src/main/java/com/example/medibook/DoctorShiftsActivity.java
@@ -2,8 +2,8 @@ package com.example.medibook;
 
 import static com.example.medibook.MainActivity.mAuth;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +13,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,21 +25,18 @@ public class DoctorShiftsActivity extends AppCompatActivity {
     Button buttonAddShifts;
     ListView listViewShifts;
 
-
-        DoctorShiftsList productsAdapter;
-
+    DoctorShiftsList productsAdapter;
 
     List<DoctorShift> doctorShiftList;
+
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_shift_day);
-
-
-
-
+        mAuth = FirebaseAuth.getInstance();
         createViews();
-
 
         buttonAddShifts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,10 +44,6 @@ public class DoctorShiftsActivity extends AppCompatActivity {
                 addShift();
             }
         });
-
-
-
-
     }
 
     private void createViews() {
@@ -65,50 +59,56 @@ public class DoctorShiftsActivity extends AppCompatActivity {
     }
 
     private void addShift() {
-
+        Log.d("DoctorShiftsActivity", "addShift() called");
+        mAuth.signInWithEmailAndPassword(AdminInbox.getInboxEmail(),AdminInbox.getInboxPassword());
         String date = editTextDate.getText().toString();
         String startTime = editTextStartTime.getText().toString();
         String endTime = editTextEndTime.getText().toString();
 
-        // Check for conflicts before adding the shift
+        Log.d("DoctorShiftsActivity", "Date: " + date + ", Start Time: " + startTime + ", End Time: " + endTime);
+
+        // Check if the user is authenticated
+        FirebaseUser current = mAuth.getCurrentUser();
+        if (current == null) {
+            // Handle the case where the user is not authenticated
+            Log.d("DoctorShiftsActivity", "User not authenticated");
+            Toast.makeText(this, "User not authenticated. Please sign in.", Toast.LENGTH_SHORT).show();
+            // Redirect to the sign-in page or take appropriate action
+            return;
+        }
+
         if (isShiftConflict(date, startTime, endTime)) {
-            // Display a message indicating the conflict
+            Log.d("DoctorShiftsActivity", "Shift conflicts with existing shifts");
             Toast.makeText(this, "Shift conflicts with existing shifts", Toast.LENGTH_SHORT).show();
         } else {
-            // If no conflict, add the shift to the list
+            Log.d("DoctorShiftsActivity", "Adding shift to the list");
 
+            DoctorShift shift = new DoctorShift(date, startTime, endTime, current.getUid());
+            doctorShiftList.add(shift);
             productsAdapter.notifyDataSetChanged();
-            FirebaseUser current = mAuth.getCurrentUser();
-            if (current != null) {
-                DoctorShift shift = new DoctorShift(editTextDate.getText().toString(),editTextStartTime.getText().toString(),editTextEndTime.getText().toString(),current.getUid());
-                MainActivity.shiftRef.child( MainActivity.shiftRef.push().getKey() ).setValue(shift);
-            }
+            MainActivity.shiftRef.child(MainActivity.shiftRef.push().getKey()).setValue(shift);
         }
     }
 
-    // Check for shift conflicts
+
+
     private boolean isShiftConflict(String newDate, String newStartTime, String newEndTime) {
         DoctorShift newShift = new DoctorShift(newDate, newStartTime, newEndTime);
-        for (DoctorShift existingShift : doctorShiftList) { // goes through every single date inputted so far
-            if (existingShift.equals(newShift) && doTimeRangesOverlap(existingShift.getStartTime(), existingShift.getEndTime(), newStartTime, newEndTime) && Integer.getInteger(newEndTime)-Integer.getInteger(newStartTime) < 30) {
+        for (DoctorShift existingShift : doctorShiftList) {
+            if (existingShift.equals(newShift) && doTimeRangesOverlap(existingShift.getStartTime(), existingShift.getEndTime(), newStartTime, newEndTime) && Integer.parseInt(newEndTime) - Integer.parseInt(newStartTime) < 30) {
                 return true;  // conflict found
             }
         }
         return false;  // No conflict found, input shift
     }
 
-    // Check if two time ranges overlap
     private boolean doTimeRangesOverlap(String start1, String end1, String start2, String end2) {
+        Log.d("DoctorShiftsActivity", "Comparing time ranges: " + start1 + " - " + end1 + " with " + start2 + " - " + end2);
 
-        return !((end1.compareTo(start2) <= 0) || (start1.compareTo(end2) >= 0)); // if end time is smaller than start time or equal then false because of conflict
+        boolean overlap = !((end1.compareTo(start2) <= 0) || (start1.compareTo(end2) >= 0));
 
+        Log.d("DoctorShiftsActivity", "Overlap result: " + overlap);
 
+        return overlap;
     }
-
-
-
-
 }
-
-
-
