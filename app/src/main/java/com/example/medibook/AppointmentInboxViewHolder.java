6d2efc9db497b64167e06251f90482aeb74144a0
Shaoze <sz.ying@icloud.com>
2023-11-12 08:37:02 -0500

    Appointment Inbox

--- app/src/main/java/com/example/medibook/AppointmentInboxViewHolder.java
+++ app/src/main/java/com/example/medibook/AppointmentInboxViewHolder.java
@@ -9,18 +9,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class AppointmentInboxViewHolder extends RecyclerView.ViewHolder {
     RelativeLayout relativeLayout;
-    TextView shiftIdView, startTimeView, endTimeView, patientUidView;
+    TextView shiftIdView, startTimeView, endTimeView, patientUidView,statusView;
     Button acceptBtn, rejectBtn;
 
     public AppointmentInboxViewHolder(View itemView) {
         super(itemView);
-        relativeLayout = itemView.findViewById(R.id.AppointmentInbox);
+        relativeLayout = itemView.findViewById(R.id.appointmentInbox);
         shiftIdView = itemView.findViewById(R.id.appointmentShiftId);
         startTimeView = itemView.findViewById(R.id.appointmentStartTime);
         endTimeView = itemView.findViewById(R.id.appointmentEndTime);
         patientUidView = itemView.findViewById(R.id.appointmentPatientUid);
+        statusView = itemView.findViewById(R.id.appointmentStatus);
         acceptBtn = itemView.findViewById(R.id.appointmentAcceptBtn);
         rejectBtn = itemView.findViewById(R.id.appointmentRejectBtn);
+
 
 
     }
