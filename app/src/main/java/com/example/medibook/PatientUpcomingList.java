692dd0efa918b39b12f406a5c6e66650da5b1ae3
Mathias Cheung <mcheu018@uottawa.ca>
2023-12-02 22:18:47 -0500

    upcoming appointment

--- app/src/main/java/com/example/medibook/PatientUpcomingList.java
+++ app/src/main/java/com/example/medibook/PatientUpcomingList.java
@@ -1,4 +1,90 @@
 package com.example.medibook;
 
-public class PatientUpcomingList {
+import static com.example.medibook.MainActivity.appointmentRef;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PatientUpcomingList extends ArrayAdapter<Appointment> {
+
+    private Activity context;
+    private List<Appointment> appointment;
+
+    public PatientUpcomingList(Activity context, List<Appointment> appointment) {
+        super(context, R.layout.activity_patient_upcoming_text, appointment);
+        this.context = context;
+        this.appointment = appointment;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            view = inflater.inflate(R.layout.activity_patient_upcoming_text, null);
+        }
+
+        // Assuming you have TextViews in your list item layout
+        TextView dateTextView = view.findViewById(R.id.editDate2);
+        TextView startTimeTextView = view.findViewById(R.id.editStartTime2);
+        TextView endTimeTextView = view.findViewById(R.id.editEndTime2);
+        Button cancel = view.findViewById(R.id.buttonCancelBooking);
+
+        // Get the DoctorShift object for this position
+        Appointment appointmentItem = appointment.get(position);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String date = dateTextView.getText().toString();
+                String startTime = startTimeTextView.getText().toString();
+                String endTime = endTimeTextView.getText().toString();
+
+                Appointment appointment1 = null;
+                for (Appointment e : appointment){
+                    if (e.getDate().equals(date) && e.getStartTime().equals(startTime) && e.getEndTime().equals(endTime)){
+                        appointment1 = e;
+
+                    }
+                }
+
+
+
+                if (appointment1 != null) {
+                    appointmentRef.child(appointment1.getUid()).removeValue();
+                    Log.d("DoctorShiftsActivity", "Deleting shift #2");
+                    appointment.remove(appointment1);
+                    notifyDataSetChanged();
+
+
+
+                }
+            }
+        });
+
+
+
+        // Set the data to the TextViews
+        dateTextView.setText(appointmentItem.getDate());
+        startTimeTextView.setText(appointmentItem.getStartTime());
+        endTimeTextView.setText(appointmentItem.getEndTime());
+
+
+
+
+        return view;
+
+
+    }
+
+
 }
